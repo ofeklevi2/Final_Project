b91d71880d466a28f7be9dfc5c12f514bc8342aa
unknown <ofeklevi2@mail.tau.ac.il>
2023-02-28 11:34:23 +0200

    Added print and free array funvtions of 1D and 2D, and finished ddg() and

--- spkmeans.c
+++ spkmeans.c
@@ -3,7 +3,29 @@
 # include <math.h>
 #include "spkmeans.h"
 
+void print_1D_Array(double *arr, int len){
+    int i;
+    for (i = 0; i < len; i++){
+        printf("%lf ", arr[i]);
+    }
+}
 
+void print_2D_Array(double **arr, int len){
+    int i, j;
+    for (i = 0; i < len; i++){
+        for (j = 0; j < len; j++){
+            printf("%lf ", arr[i][j]);
+        }
+        printf("\n");
+    }
+}
+
+void free_2D_Array(double **arr, int len){
+    int i, j;
+    for (i = 0; i < len; i++){
+        free(arr[i]);
+    }
+}
 
 double distance(double *xi, double *xj, int len){
     int i;
@@ -37,11 +59,41 @@ double **wam(double **dataPoints, int len){
     return w;
 }
 
-void main(){
-    double *x1 = malloc(3 * sizeof(double));
-    double *x2 = malloc(3 * sizeof(double));
-    x1[0] = 1.0, x1[1] = 2., x1[2] = 3.;
-    x2[0] = 2., x2[1] = 3., x2[2] = 4.;
-    double res = distance(x1, x2, 3);
-    printf("%lf\n", res);
+double **ddg(double **dataPoints, int len){
+    int i, j;
+    double **w = wam(dataPoints, len);
+    double **diagonal_Degree_Matrix = (double**)malloc(len * sizeof(double*));
+    for (int i =0; i < len; i++){
+        diagonal_Degree_Matrix[i] = (double*)calloc(len, sizeof(double));
+    }
+    for (int i =0; i < len; i++){
+        double sum = 0;
+        for (int j =0; j < len; j++){
+            sum += w[i][j];
+        }
+
+        diagonal_Degree_Matrix[i][i] = sum;
+    }
+    return diagonal_Degree_Matrix;
+}
+
+double **gl(double **dataPoints, int len){
+    int i, j;
+    double **w, **dd_Matrix, **gl_Matrix;
+    gl_Matrix = (double**)malloc(len * sizeof(double*));
+    for (i = 0; i < len; i++){
+        gl_Matrix[i] = (double*)malloc(len * sizeof(double));
+    }
+    w = wam(dataPoints, len);
+    dd_Matrix = ddg(dataPoints, len);
+    for (i = 0; i < len; i++){
+        for (j = 0; j < len; j++){
+            gl_Matrix[i][j] = dd_Matrix[i][j] - w[i][j];
+        }
+    }
+    return gl_Matrix;
+}
+
+int main(){
+    return 0;
 }
