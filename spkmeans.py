a5176259481947b79c45926353cbcf1c8fb88efe
USER <USER@MacBook-Pro.local>
2023-03-05 18:58:06 +0200

    done working on python and c module files

--- spkmeans.py
+++ spkmeans.py
@@ -14,31 +14,58 @@ def eigengapHuristic(eigenvalues):
     relevant_deltas = np.array(deltas[:math.floor(n/2)])
     return np.argmax(relevant_deltas)
 
-def draw_initial_centroids(K, dataPoints):
-    pass
+def draw_new_index(data,centroids,indexes):
+    def D(vec,centroids):
+        distances = [np.linalg.norm(vec - centroids[i]) for i in range(len(centroids))]
+        return np.min(distances)
+    
+    D_arr = [D(vec, centroids) for vec in data]
+    D_sum = np.sum(D_arr)
+    probabilities = [D_arr[i]/D_sum for i in range(len(data))]
+    return np.random.choice(indexes, p=probabilities)
 
+def draw_initial_centroids(K,dataPoints):
+  indexes = range(len(dataPoints))
+  selected_indexes = []
+  centroids = []
+  new_index = np.random.choice(indexes)
+  selected_indexes.append(new_index)
+  centroids.append(dataPoints[new_index].tolist())
+  while len(centroids) < K:
+    new_index = draw_new_index(dataPoints, centroids, indexes)
+    selected_indexes.append(new_index)
+    centroids.append(dataPoints[new_index].tolist())
+  return selected_indexes,centroids
 
-def format_print(centroids):
-    for centroid in centroids:
+def format_print(elements, isInts = False):
+    if (isInts):
+        c=""
+        for elem in elements:
+            c += str(elem)+","
+        c = c[:-1]
+        print(c)
+        return 
+
+    for elem in elements:
         c = ""
-        for cord in range(len(centroid)-1):
-            x = "%.4f"%centroid[cord]
+        for cord in range(len(elem)-1):
+            x = "%.4f"%elem[cord]
             c += x +','
-        c += "%.4f"%centroid[cord+1]
+        c += "%.4f"%elem[cord+1]
         print(c)
 
 def main():
     print("main")
     args = sys.argv[1:]
     if (len(args) == 3):
-        K = args[0]
+        K = int(args[0])
         goal = args[1]
         input_data = args[2]
     else:
         goal = args[0]
         input_data = args[1]
         
-    dataPoints = np.loadtxt(input_data,delimiter=",",dtype=float)
+    dataPoints = np.loadtxt(input_data,delimiter=",",dtype=float) 
     dataPoints = dataPoints.tolist()
     if (goal == "wam"):
         res = mykmeanssp.wam(dataPoints, len(dataPoints))
@@ -46,9 +73,8 @@ def main():
         res = mykmeanssp.ddg(dataPoints, len(dataPoints))
     elif (goal == "gl"):
         res = mykmeanssp.gl(dataPoints, len(dataPoints))
-    elif (goal == "jacobi"){
+    elif (goal == "jacobi"):
         res = mykmeanssp.jacobi(dataPoints, len(dataPoints))
-    }
     elif (goal == "spk"):
         gl = mykmeanssp.gl(dataPoints,len(dataPoints))
         jacobi = mykmeanssp.jacobi(gl, len(gl))
@@ -56,8 +82,13 @@ def main():
             K = eigengapHuristic(jacobi[0])
         spectral_kmeans_vectors = np.array(jacobi[1:K+1])
         spectral_kmeans_vectors = np.transpose(spectral_kmeans_vectors)
-        chosen_centroids, initial_centroids = draw_initial_centroids(K, spectral_kmeans_vectors)
-        #res = spk(initial_centroids, kmeans_vectors, len(kmeans_vectors), len(kmeans_vectors[0]))
+        selected_indexes, initial_centroids = draw_initial_centroids(K, spectral_kmeans_vectors)
+        spectral_kmeans_vectors = spectral_kmeans_vectors.tolist()
+        for centroid in initial_centroids:
+            spectral_kmeans_vectors.remove(centroid)
+        dataPoints = initial_centroids + spectral_kmeans_vectors
+        format_print(selected_indexes, isInts =True)
+        res = mykmeanssp.spk(K, dataPoints ,len(dataPoints), len(dataPoints[0]))
     format_print(res)
 
 
