f2e7560d1732ff32c13a85508f33de63dca2459e
USER <USER@MacBook-Pro.local>
2023-03-05 12:53:31 +0200

    jacobi in py

--- spkmeans.py
+++ spkmeans.py
@@ -1,11 +1,22 @@
 import numpy as np
 import pandas as pd
 import mykmeanssp
+import math
 import sys
 np.random.seed(0)
 
-def eigengapHuristic():
+def eigengapHuristic(eigenvalues):
+    n = len(eigenvalues)
+    if (n <= 1):
+      return 0
+    eigenvalues.sort()
+    deltas = [abs(eigenvalues[i]-eigenvalues[i+1]) for i in range(len(eigenvalues)-1)]
+    relevant_deltas = np.array(deltas[:math.floor(n/2)])
+    return np.argmax(relevant_deltas)
+
+def draw_initial_centroids(K, dataPoints):
     pass
+
 
 def format_print(centroids):
     for centroid in centroids:
@@ -24,7 +35,6 @@ def main():
         goal = args[1]
         input_data = args[2]
     else:
-        K = eigengapHuristic()
         goal = args[0]
         input_data = args[1]
         
@@ -36,6 +46,18 @@ def main():
         res = mykmeanssp.ddg(dataPoints, len(dataPoints))
     elif (goal == "gl"):
         res = mykmeanssp.gl(dataPoints, len(dataPoints))
+    elif (goal == "jacobi"){
+        res = mykmeanssp.jacobi(dataPoints, len(dataPoints))
+    }
+    elif (goal == "spk"):
+        gl = mykmeanssp.gl(dataPoints,len(dataPoints))
+        jacobi = mykmeanssp.jacobi(gl, len(gl))
+        if (len(args) == 2):
+            K = eigengapHuristic(jacobi[0])
+        spectral_kmeans_vectors = np.array(jacobi[1:K+1])
+        spectral_kmeans_vectors = np.transpose(spectral_kmeans_vectors)
+        chosen_centroids, initial_centroids = draw_initial_centroids(K, spectral_kmeans_vectors)
+        #res = spk(initial_centroids, kmeans_vectors, len(kmeans_vectors), len(kmeans_vectors[0]))
     format_print(res)
 
 
